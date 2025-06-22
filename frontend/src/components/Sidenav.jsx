4cddbc23796997c220f328302f29e47a45e82231
Wilson Oyem <85078140+oyemwilson@users.noreply.github.com>
2025-06-22 13:06:08 +0100

    Update Sidenav.jsx
    logo update on sidenav

--- frontend/src/components/Sidenav.jsx
+++ frontend/src/components/Sidenav.jsx
@@ -40,13 +40,13 @@ const Sidenav = () => {
                 </svg>
               </button>
               <a href="https://flowbite.com" className="flex ms-2 md:me-24">
-                <img
-                  src="./images/logo3.webp"
+                {/* <img
+                  src="../../logo3.webp"
                   className="h-8 me-3"
-                  alt="FlowBite Logo"
-                />
+                  alt=""
+                /> */}
                 <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
-                  Crypto
+                  Crypto Dashboard
                 </span>
               </a>
             </div>
