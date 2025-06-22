a63d2db07706c0a8827774e65004898da652fb9d
Wilson Oyem <85078140+oyemwilson@users.noreply.github.com>
2025-06-22 13:10:37 +0100

    logo fix

--- frontend/src/components/Sidenav.jsx
+++ frontend/src/components/Sidenav.jsx
@@ -40,13 +40,13 @@ const Sidenav = () => {
                 </svg>
               </button>
               <a href="https://flowbite.com" className="flex ms-2 md:me-24">
-                {/* <img
-                  src="../../logo3.webp"
+                <img
+                  src="/logo3.webp"
                   className="h-8 me-3"
                   alt=""
-                /> */}
+                />
                 <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
-                  Crypto Dashboard
+                  BITRUN
                 </span>
               </a>
             </div>
